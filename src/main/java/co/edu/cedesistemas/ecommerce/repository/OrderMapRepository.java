7184761201e228bd3c3c92660f6c2dde50c4c049
@juanferatl <ferna283@hotmail.com>
2020-06-21 22:51:47 -0500

    Solución taller 6, Spring IoC. Punto b.

---
+++ src/main/java/co/edu/cedesistemas/ecommerce/repository/OrderMapRepository.java
@@ -0,0 +1,19 @@
+package co.edu.cedesistemas.ecommerce.repository;
+
+import co.edu.cedesistemas.ecommerce.model.Store;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OrderMapRepository extends AbstractMapRepository<Store, String> {
+    public OrderMapRepository(Map<String, Store> repository) {
+        super(repository);
+    }
+
+    public Set<Store> findByName(final String name) {
+        return repository.values().stream()
+                .filter(s -> s.getName().contains(name))
+                .collect(Collectors.toSet());
+    }
+}
