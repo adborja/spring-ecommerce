d37acc04be9383a3c710566377a25461a356f39e
Jhonatan Plata <jplata@bancolombia.com.co>
2020-06-26 18:41:14 -0500

    Avance punto 1

---
+++ src/main/java/co/edu/cedesistemas/ecommerce/repository/OrderItemRepository.java
@@ -0,0 +1,6 @@
+package co.edu.cedesistemas.ecommerce.repository;
+
+import co.edu.cedesistemas.ecommerce.model.OrderItem;
+
+public interface OrderItemRepository extends Repository<OrderItem, String> {
+}
---
+++ src/main/java/co/edu/cedesistemas/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package co.edu.cedesistemas.ecommerce.repository;
+
+import co.edu.cedesistemas.ecommerce.model.Product;
+
+public interface ProductRepository extends Repository<Product, String> {
+}
