d01f9c751d159f9005ec708f8bc8fd0f532a5026
Jhonatan Plata <jplata@bancolombia.com.co>
2020-06-26 17:12:03 -0500

    configuracion docker mac

--- run_mysql.sh
+++ run_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --name mysql.cedesistemas.local \
-        -v /var/data/mysql:/var/lib/mysql \
+        -v /Users/userguest/cedesistemas/data/mysql:/var/lib/mysql \
         -p 3306:3306 \
         -e MYSQL_ROOT_PASSWORD=root \
         -d mysql:5.7
