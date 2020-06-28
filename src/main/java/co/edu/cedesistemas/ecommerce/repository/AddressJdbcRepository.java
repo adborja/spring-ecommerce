0ae7b754170416e1589c3555ad36e61b2538204d
@juanferatl <ferna283@hotmail.com>
2020-06-28 18:15:07 -0500

    Taller Lección 7 – Spring JDBC Module Punto b.

---
+++ src/main/java/co/edu/cedesistemas/ecommerce/repository/AddressJdbcRepository.java
@@ -0,0 +1,41 @@
+package co.edu.cedesistemas.ecommerce.repository;
+
+import co.edu.cedesistemas.ecommerce.model.Address;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.List;
+
+public class AddressJdbcRepository implements AddressRepository
+{
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public AddressJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public List<Address> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public <S extends Address> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public Address findById(String s) {
+        return null;
+    }
+
+    @Override
+    public void remove(String s) {
+
+    }
+
+    @Override
+    public Iterable<Address> findAll() {
+        return null;
+    }
+}
