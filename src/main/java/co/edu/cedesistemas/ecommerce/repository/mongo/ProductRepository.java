82f4c0ef9d3bd434fb4d8c60ecc6d605b092bdb4
Leidyrpo <leidyrpo@gmail.com>
2020-07-04 18:34:54 -0500

    Implementación Taller 9

---
+++ src/main/java/co/edu/cedesistemas/ecommerce/repository/mongo/ProductRepository.java
@@ -0,0 +1,13 @@
+package co.edu.cedesistemas.ecommerce.repository.mongo;
+
+import co.edu.cedesistemas.ecommerce.model.document.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findByNameLike(String name);
+    List<Product> findByDescriptionLike(String descripcion);
+}
--- src/main/java/co/edu/cedesistemas/ecommerce/service/ProductService.java
+++ src/main/java/co/edu/cedesistemas/ecommerce/service/ProductService.java
@@ -1,9 +1,11 @@
 package co.edu.cedesistemas.ecommerce.service;
 
-import co.edu.cedesistemas.ecommerce.model.Product;
-import co.edu.cedesistemas.ecommerce.repository.ProductRepository;
+
+import co.edu.cedesistemas.ecommerce.model.document.Product;
+import co.edu.cedesistemas.ecommerce.repository.mongo.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,6 +19,25 @@ public class ProductService {
     public Product createProduct(Product product) {
         product.setId(UUID.randomUUID().toString());
         return repository.save(product);
+    }
+    public Product getById(final String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Product> getByName(String name) {
+        return repository.findByNameLike(name);
+    }
+
+    public List<Product> getByDescription(String description) {
+        return repository.findByDescriptionLike(description);
+    }
+
+    public Iterable<Product> getAllProduct() {
+        return repository.findAll();
+    }
+
+    public void deleteById(final String id) {
+        repository.deleteById(id);
     }
 
 }
