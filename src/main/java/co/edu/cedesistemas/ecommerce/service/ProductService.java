92793947d5eb5f23e7ca94f9ddc955997015fb11
Jhonatan Plata <jplata@bancolombia.com.co>
2020-07-04 09:38:50 -0500

    Taller 9 Punto B, C y D

---
+++ src/main/java/co/edu/cedesistemas/ecommerce/service/ProductService.java
@@ -0,0 +1,43 @@
+package co.edu.cedesistemas.ecommerce.service;
+
+import co.edu.cedesistemas.ecommerce.model.document.Product;
+import co.edu.cedesistemas.ecommerce.repository.mongo.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+    private final ProductRepository repository;
+
+    public ProductService(final ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public Product createProduct(Product p) {
+        p.setId(UUID.randomUUID().toString());
+        return repository.save(p);
+    }
+
+    public Product getById(final String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void delete(final String id) {
+        repository.deleteById(id);
+    }
+
+    public List<Product> getByName(final String name) {
+        return repository.findByNameLike(name);
+    }
+
+    public List<Product> getByDescription(final String description) {
+        return repository.findByDescriptionLike(description);
+    }
+    public Iterable<Product> getAllStores() {
+        return repository.findAll();
+    }
+
+}
