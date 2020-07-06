9991747703251bebfc2244996072b824bb5ea808
kathe20m <kathe20m@hotmail.com>
2020-07-05 23:33:58 -0500

    leccion 7 - docker hands on

--- run_mysql.sh
+++ run_mysql.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-docker run --restart=unless-stopped \
-        --name mysql.cedesistemas.local \
-        -v /var/data/mysql:/var/lib/mysql \
-        -p 3306:3306 \
-        -e MYSQL_ROOT_PASSWORD=root \
-        -d mysql:5.7
+docker run --restart=unless-stopped --name mysql.cedesistemas.local -v /var/data/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7
