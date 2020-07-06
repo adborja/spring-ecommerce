1c51052a91602d1b510acd19b483c3cfb789f940
yesolano <yenifers16@hotmail.com>
2020-07-05 23:23:13 -0500

    Parte taller 9

---
+++ src/main/java/co/edu/cedesistemas/ecommerce/controller/ProductController.java
@@ -0,0 +1,7 @@
+package co.edu.cedesistemas.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+}
--- src/main/java/co/edu/cedesistemas/ecommerce/service/ProductService.java
+++ src/main/java/co/edu/cedesistemas/ecommerce/service/ProductService.java
@@ -1,7 +1,9 @@
 package co.edu.cedesistemas.ecommerce.service;
 
-import co.edu.cedesistemas.ecommerce.model.Product;
-import co.edu.cedesistemas.ecommerce.repository.ProductRepository;
+import co.edu.cedesistemas.ecommerce.model.document.Product;
+import co.edu.cedesistemas.ecommerce.repository.mongo.ProductRepository;
+
+import java.util.List;
 import java.util.UUID;
 
 public class ProductService {
@@ -12,8 +14,18 @@ public class ProductService {
         this.repository= repository;
     }
 
-    public Product createProduct(Product product){
+    public Product createProduct(Product product) {
         product.setId(UUID.randomUUID().toString());
         return repository.save(product);
     }
+
+    public List<Product> getByName(final String name){
+        return repository.findByNameLike(name);
+    }
+
+    public List<Product> getByDescription(final String description){
+        return repository.findByDescriptionLike(description);
+    }
+
+
 }
