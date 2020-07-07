706550e7801303b55865e7d910ad1f40ec9a7038
Diana Durango Zapata <ddurango@bancolombia.com.co>
2020-07-06 20:10:47 -0500

    taller 9 punto b

--- src/main/java/co/edu/cedesistemas/ecommerce/repository/mongo/ProductRepository.java
+++ src/main/java/co/edu/cedesistemas/ecommerce/repository/mongo/ProductRepository.java
@@ -2,9 +2,11 @@ package co.edu.cedesistemas.ecommerce.repository.mongo;
 
 import co.edu.cedesistemas.ecommerce.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
     List<Product> findByNameLike(String name);
     List<Product> findByDescriptionLike(String description);
